0adfa6de7784b3f85177d6a89a2068ba51503105
Cybertek Systems <cyberteksys@gmail.com>
2017-12-26 06:33:03 -0800

    Add catchall route and rescuer from no record for protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :render_root
+
+  private
+
+  def render_root
+    redirect_to root_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :images
-  post '/editor' => 'images#editor', as: 'editor_post'
-  get '/editor' => 'images#editor', as: 'editor_get'
-
   root 'images#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '*path' => redirect('/'), via: :all
 end
