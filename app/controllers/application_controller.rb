ffd04f5fd32105218b0085508ae8cfbe69a29480
Cybertek Systems <cyberteksys@gmail.com>
2017-12-28 02:03:48 -0800

    Change image validations, fix header code, and add unsplash initializer for future implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, with: :render_root
-  after_action :set_access_control_headers
   
   private
-
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = "http://allpic.site"
-    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-  end
 
   def render_root
     redirect_to root_path
---
+++ config/initializers/unsplash_config.rb
@@ -0,0 +1,5 @@
+Unsplash.configure do |config|    
+    config.application_id     = "178f8c2615f315ef2ede3ea08c99e6c25ead104fe6b459f710ab59e277acb86e"    
+    config.application_secret = "de2982e166312f620823f5f0e163049748b54db3467dfcf3fd2abf450d037a12"
+    config.utm_source = "allpic.site"
+  end
